bfb6884fc142a7de1ceb606d5e013574ece6fcb2
Guruprasada Shridhar Hegde <gshegde6@gmail.com>
2024-01-15 11:08:54 +0530

    Added mutable and immutable concept

---
+++ mutabVsImmute.py.py
@@ -0,0 +1,15 @@
+# All are the example for mutable objects in python
+# List Example
+my_list = ['1','guru','prasad']
+my_list[1] = 'boss'
+print(my_list)
+
+# Dictionary Example
+my_dict = {'name':'guru','age':'25'}
+my_dict['name'] = 'big boss'
+print(my_dict)
+
+# Set Example
+my_set = {1,2,3}
+my_set.add(4)
+print(my_set)
