ecded84ea65e55bedcd948e4fae479b5534bd8ff
GuruprasadaShridharHegde <gshegde6@gmail.com>
2024-04-14 17:22:50 +0530

    all done'

---
+++ Strings_2.py
@@ -0,0 +1,6 @@
+print(len('hello'))
+mystring = 'abcdef'
+print(mystring[:3]) # Starting to 3
+print(mystring[::2]) # last is a Step size
+print(mystring[2:6:3])
+print(mystring[::-1])
---
+++ immutability.py
@@ -0,0 +1,16 @@
+# name = "samsung"
+# name[0] = "k" # This is the example for immutabilitym
+
+# name2 = ["abc","1","3"]
+# name2[0] = "0"
+
+# name3 = "123"
+# name4 = 123
+# name5 = name3 + name4
+
+name6 = {"a":1,"b":"2","c":3}
+name6["b"]="4"
+print("name6",name6)
+
+name7 = [1,2,3,4]
+name7[6] = "6"
