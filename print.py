6338fde15ba9e03ed0884af95cfb07548e933d4a
GuruprasadaShridharHegde <gshegde6@gmail.com>
2024-05-16 22:10:17 +0530

    added print expressions

--- print.py
+++ print.py
@@ -2,3 +2,24 @@ mystate= "here"
 print("coming {}".format(mystate))
 
 print("animals are {} {} {}".format('python','rat','cat'))
+
+print("animals are {1} {0} {2}".format('python','rat','cat'))
+
+print("animals are {h} {k} {c}".format(c='python',h='rat',k='cat'))
+
+
+result = 10/4
+print(result)
+print("result is {}".format(int(result)))
+
+# Adding a precession
+match = 100/777
+print(match)
+print("the result was {r:1.5f}".format(r=match))
+
+
+kinger = 104.333
+print("result is {r:10.3f}".format(r=kinger))
+
+# Example print formats
+# https://pyformat.info/
